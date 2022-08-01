4ba185b84e02eb905fe312bf532a6047988358c2
Sanjoy Roy <49688614+Sanjoy5i@users.noreply.github.com>
2022-08-01 14:03:19 +0600

    first commit

---
+++ src/conponents/Countries.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {v4 as uuidv4} from "uuid";
+import Country from './Country';
+import style from './countries.module.css'
+
+export default function Countries(props) {
+  return (
+    <section className={style.countries}>
+        {props.countries.map((country)=>{
+            const countryNew={
+                country,
+                id:uuidv4(),
+            }
+            // console.log(countryNew);
+            return <Country {...countryNew} key={countryNew.id} onRemoveCountry={props.onRemoveCountry}/>
+        })}
+    </section>
+  )
+}
---
+++ src/conponents/Search.js
@@ -0,0 +1,17 @@
+import React,{useState,useEffect} from 'react'
+
+export default function Search(props) {
+    const [searchText, setSearchText] = useState("")
+    const handleChange=(e)=>{
+        setSearchText(e.target.value);
+    }
+    useEffect(() => {
+      props.onSearch(searchText)
+    }, [searchText])
+    
+  return (
+    <div style={{textAlign:"center"}}>
+        <input type="text" placeholder='Search Country' value={searchText} onChange={handleChange}/>
+    </div>
+  )
+}
