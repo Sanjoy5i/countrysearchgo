97381aa825baa09bd5caf3b7cc1dbd29bc5d5d2f
Sanjoy Roy <49688614+Sanjoy5i@users.noreply.github.com>
2022-08-01 14:07:42 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import App2 from './App2';
-// import App3 from './App3';
-// import App4 from './App4';
 
 
 ReactDOM.render(
